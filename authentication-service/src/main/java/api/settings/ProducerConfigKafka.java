ba1416ee7fad9d632e644ad2ffed0528cc059823
jvitor07 <jvitoroliveira0711@gmail.com>
2023-04-02 20:10:23 -0300

    feature(backend): Adiciona as configuração do tópico inicial

--- authentication-service/src/main/java/api/settings/ProducerConfigKafka.java
+++ authentication-service/src/main/java/api/settings/ProducerConfigKafka.java
@@ -1,12 +1,15 @@
 package api.settings;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
@@ -30,5 +33,16 @@ public class ProducerConfigKafka {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        HashMap<String, Object> configurations = new HashMap<>();
+        configurations.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configurations);
+    }
+
+    public NewTopic setupTopics() {
+        return new NewTopic("greetings-email", 10, Short.parseShort("1"));
     }
 }
