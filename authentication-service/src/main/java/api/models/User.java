0b11d17ee8c223cf43965300eb4bff0ac9ee24c1
jvitor07 <jvitoroliveira0711@gmail.com>
2023-04-02 14:54:32 -0300

    feature(backend): Cria modelo de usuários

---
+++ authentication-service/src/main/java/api/models/User.java
@@ -0,0 +1,43 @@
+package api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(schema = "APPLICATION", name = "TB_USERS")
+@Entity
+public class User {
+    @Id
+    @SequenceGenerator(name = "SQ_USERS", schema = "APPLICATION", sequenceName = "SQ_USERS")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_USERS")
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password);
+    }
+}
