6544c4e28f443a27eec2085551e23fce61793cbc
jvitor07 <jvitoroliveira0711@gmail.com>
2023-04-02 17:09:36 -0300

    feature(backend): Implementa o endpoint de registro no AuthController

---
+++ authentication-service/src/main/java/api/controllers/v1/AuthController.java
@@ -0,0 +1,40 @@
+package api.controllers.v1;
+
+import api.dtos.requests.UserRegisterRequestDTO;
+import api.dtos.responses.ResponseDTO;
+import api.models.User;
+import api.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/auth")
+@RestController
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<ResponseDTO<User>> register(@RequestBody @Valid UserRegisterRequestDTO request) {
+        try {
+            this.userService.uniqueEmailCheck(request.getEmail());
+            User result = this.userService.create(request.convert());
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new ResponseDTO<>(result, null));
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity
+                    .status(ex.getStatusCode())
+                    .body(new ResponseDTO<>(null, List.of(ex.getMessage())));
+        }
+    }
+}
