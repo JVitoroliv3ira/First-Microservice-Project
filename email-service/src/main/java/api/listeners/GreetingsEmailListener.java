6a8db7df9c79097250965daf656c3c4dc3e1d36a
jvitor07 <jvitoroliveira0711@gmail.com>
2023-04-02 22:39:34 -0300

    feature(backend): Implementa método para enviar o email de boas vindas

--- email-service/src/main/java/api/listeners/GreetingsEmailListener.java
+++ email-service/src/main/java/api/listeners/GreetingsEmailListener.java
@@ -1,6 +1,9 @@
 package api.listeners;
 
+import api.services.GreetingsEmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +11,40 @@ import org.springframework.stereotype.Component;
 @Component
 public class GreetingsEmailListener {
 
+    private final GreetingsEmailService emailService;
+    private final String host;
+    private final Integer port;
+    private final String email;
+    private final String username;
+    private final String password;
+    private final String protocol;
+
+    public GreetingsEmailListener(
+            GreetingsEmailService emailService,
+            @Value("${email-property.host}") String host,
+            @Value("${email-property.port}") Integer port,
+            @Value("${email-property.email}") String email,
+            @Value("${email-property.username}") String username,
+            @Value("${email-property.password}") String password,
+            @Value("${email-property.protocol}") String protocol
+    ) {
+        this.emailService = emailService;
+        this.host = host;
+        this.port = port;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.protocol = protocol;
+    }
+
     @KafkaListener(topics = "greetings-email", groupId = "greetings-email-group")
-    public void listen(String message) {
-        log.info("Thread: {}", Thread.currentThread());
-        log.info("Mensagem: {}", message);
+    public void listen(String to) {
+        this.emailService.setupMailSender(this.host, this.port, this.username, this.password, this.protocol);
+        this.emailService.send(
+                this.email,
+                to,
+                "Bem-vindo(a) à nossa aplicação!",
+                String.format("Olá %s, seja bem-vindo(a) à nossa aplicação. Estamos felizes em tê-lo(a) aqui!", to)
+        );
     }
 }
